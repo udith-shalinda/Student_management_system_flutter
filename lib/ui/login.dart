7a7751ec4bb45e482f0a8162f7fc6540f2cdf69c
Udith <udithshalinda2@gmail.com>
2019-07-24 09:38:44 +0530

    first commit

---
+++ lib/ui/login.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+
+  bool incorrectPassword  = false;
+  var _formKey = GlobalKey<FormState>();
+  String _email;
+  String _formpassword;
+  String userKey;
+
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(52, 66, 86, .7),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(56, 66, 86, 1),
+        title: new Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 25,
+            ),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(Icons.person_add),
+              onPressed: signUp,
+              )
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+//          new Center(
+//            child: new Image.asset(
+//              'images/cover_two.jpg',
+//              fit: BoxFit.fill,
+//              width: 500,
+//              height: 1000,
+//            ),
+//          ),
+          new Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    validator: (value){
+                      String p = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                      RegExp regExp = new RegExp(p);
+                      if(value.length == 0 || !regExp.hasMatch(value)){
+                        return "Invalid email";
+                      }
+                    },
+                    onSaved: (value){
+                      _email = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Enter the email",
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide(
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    obscureText: true,
+                    validator: (value){
+
+                      if(value.length == 0){
+                        return "Password is empty";
+                      }
+                    },
+                    onSaved: (value){
+                      _formpassword = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Enter the password",
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      errorText: incorrectPassword ? "User email or password is incorrect":null,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: loginButton ,
+                  color: Color.fromRGBO(52, 66, 86, 1),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontSize: 20,
+                    ),
+                  ),
+                  textColor: Colors.white70,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // void getTheUser(Event event){
+  //    User user = User.fromSnapshot(event.snapshot);
+  //    if(_email == user.email){
+  //      userKey = user.key;
+  //      print(user.email);
+  //    }
+    
+  // }
+
+  void signUp(){
+    var router = new MaterialPageRoute(
+        builder: (BuildContext context){
+          // return new SignUp();
+        });
+    Navigator.of(context).push(router);
+  }
+
+  void loginButton(){
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      // databaseReference.onChildAdded.listen(getTheUser);
+      signInWithCredentials(_email, _formpassword);
+    }
+  }
+
+  void signInWithCredentials(String email, String password) async {
+  //   FirebaseUser user;
+  //   final prefs = await SharedPreferences.getInstance();   //save username
+  //   try{
+  //     user = await _firebaseAuth.signInWithEmailAndPassword(
+  //         email: email,
+  //         password: password
+  //     );
+  //   }catch(e){
+  //     print(e.toString());
+  //   }finally{
+  //     if(user != null){
+  //       incorrectPassword = false;
+  //       prefs.setString("userEmail", _email);
+  //       prefs.setString("userKey", userKey);
+  //       var router = new MaterialPageRoute(
+  //           builder: (BuildContext context){
+  //             return new Home();
+  //           });
+  //       Navigator.of(context).push(router);
+  //     }else{
+  //       print("Authentication failed");
+  //       setState(() {
+  //         incorrectPassword = true;
+  //       });
+  //     }
+  //   }
+  }
+}
